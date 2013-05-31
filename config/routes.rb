6bdbc370eca4892aae4418b8524d117e720fd7c1
Gustavo Porpino <gporpino@icloud.com>
2013-05-31 20:00:21 -0300

    subindo tudo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 PftSendMail::Application.routes.draw do
+  
+  resources :students
+
+  root :to => "students#index"
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
